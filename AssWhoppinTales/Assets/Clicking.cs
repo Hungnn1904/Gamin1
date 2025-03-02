c2292b50106c3bcf302fc4bf9b8013e1b2047d2a
SsumB1tcheS <nhathung19112004@gmail.com>
2025-03-03 00:19:35 +0700

    1st step is done
    done with the main menu, intro and the credits scene, tho the button function dont work in credits for some reason

---
+++ AssWhoppinTales/Assets/Clicking.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Clicking : MonoBehaviour
+{
+    public  void Skipping1(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public  void Skipping2(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
+    }
+
+    public  void Backing1(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+    }
+    public  void Backing2(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-2);
+    }
+}
