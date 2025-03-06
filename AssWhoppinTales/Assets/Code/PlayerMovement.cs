ffcf2b47def43fc1addd5ae66ba1fdb3cf8021bf
SsumB1tcheS <nhathung19112004@gmail.com>
2025-03-06 19:22:40 +0700

    movement wooooo

---
+++ AssWhoppinTales/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private float X, Y;
+    
+    private Rigidbody2D bd;
+    
+    //handling animations
+    private Animator animator;
+
+    void Start()
+    {
+        bd = GetComponent<Rigidbody2D>();
+
+        // Get the Animator component 
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        X = Input.GetAxisRaw("Horizontal");
+        Y = Input.GetAxisRaw("Vertical");
+
+        // If moving
+        bool isWalking = (X != 0 || Y != 0);
+
+        // Update the "isWalking" for switching between idle and walk
+        animator.SetBool("isWalking", isWalking);
+
+        // Update movement input values for the Blend Tree
+        animator.SetFloat("InputX", X);
+        animator.SetFloat("InputY", Y);
+
+        // Store the last movement direction when stopping(for idle direction)
+        if (!isWalking)
+        {
+            animator.SetFloat("LastKeyPressX", X);
+            animator.SetFloat("LastKeyPressY", Y);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //so that you can use fast 1399 movement 
+        Vector2 movement = new Vector2(X, Y).normalized * movementSpeed;
+
+        bd.linearVelocity = movement; //  This could be `bd.velocity`, aint trying it tho
+    }
+}
